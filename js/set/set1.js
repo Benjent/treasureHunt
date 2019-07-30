926e282137a8b024b72e8c5bd9aaf2cdba75bf2e
Benjent <Benjent@hotmail.fr>
2019-07-30 12:25:37 +0200

    Make sure app works with any player

--- js/set/set1.js
+++ js/set/set1.js
@@ -6,6 +6,7 @@ const TS1 = new TreasureSpot(4, 4, 1);
 const TS2 = new TreasureSpot(4, 5, 2);
 const A1 = new Adventurer(9, 9, 'N', 'Benjent', 'GAAADAAAAAAADADA');
 const A2 = new Adventurer(9, 2, 'N', 'Lara', 'DDAAAAADAA');
+const A3 = new Adventurer(3, 2, 'S', 'Indiana', 'AGADAADAA');
 // Start map
 // X X X X X X X X X X
 // X M X X X X X X X X
@@ -29,4 +30,4 @@ const A2 = new Adventurer(9, 2, 'N', 'Lara', 'DDAAAAADAA');
 // . X X X X X X X X X
 // . X X X X X X X X X
 // . . . . . . . . . .
-const gameSet = new GameSet(C, [M1, M2, M3], [TS1, TS2], [A1, A2]);
+const gameSet = new GameSet(C, [M1, M2, M3], [TS1, TS2], [A1, A2, A3]);
